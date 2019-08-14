8755e129bba72fd0452c8248158583ec2f9ecfd8
yapie <targaryen.akane@gmail.com>
2019-08-14 18:02:38 +0700

    clean up

--- src/list.js
+++ src/list.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
 const { sourceGitignore } = require('./helper');
 
-// const CURR_DIR = process.cwd();
-
 function list() {
   const files = fs.readdirSync(sourceGitignore);
   const f = files.filter(
     i =>
-      i !== 'README.md' ||
-      i !== '.github' ||
-      i !== '.git' ||
-      i !== 'CONTRIBUTING' ||
-      i !== 'LICENSE' ||
+      i !== 'README.md' &&
+      i !== '.github' &&
+      i !== '.git' &&
+      i !== 'CONTRIBUTING' &&
+      i !== 'LICENSE' &&
       i !== '.travis.yml'
   );
+
   f.forEach(i => {
     const x = i.split('.');
     console.log(x[0].toLowerCase());
