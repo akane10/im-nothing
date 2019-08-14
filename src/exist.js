ed2c58e52cff3c59e502d89e272a060ae1ee44ef
yapie <targaryen.akane@gmail.com>
2019-08-15 01:50:55 +0700

    tidy up (exist.js)

--- src/exist.js
+++ src/exist.js
@@ -3,12 +3,14 @@ const { sourceGitignore, searchFile } = require('./helper');
 
 function exist(languages) {
   const files = fs.readdirSync(sourceGitignore);
-  const GitignoreFileNames = files.filter(searchFile(languages));
 
-  const names = GitignoreFileNames.map(i => {
-    const [name] = i.split('.');
-    return name.toLowerCase();
-  });
+  const splitFileName = i => i.split('.');
+  const toLower = ([name]) => name.toLowerCase();
+
+  const names = files
+    .filter(searchFile(languages))
+    .map(splitFileName)
+    .map(toLower);
 
   if (names.length === 0) return console.log('nothing exists');
 
