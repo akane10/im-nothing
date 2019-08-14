417664bbb345f56356dd14dba2746f02a5853c07
yapie <targaryen.akane@gmail.com>
2019-08-15 02:07:44 +0700

    tidy up (add.js)

--- src/add.js
+++ src/add.js
@@ -1,25 +1,22 @@
 const fs = require('fs');
 const { CURR_DIR, sourceGitignore, searchFile } = require('./helper');
 
-function getFile(languages) {
-  const filesInFolder = fs.readdirSync(sourceGitignore);
-  return filesInFolder.filter(searchFile(languages));
-}
-
 function writing(files) {
   const report = [];
+
+  const pathToAppend = `${CURR_DIR}/.gitignore`;
+  const appendContents = (path_, contents) =>
+    fs.appendFileSync(path_, contents);
 
   files.forEach(i => {
     const contents = fs.readFileSync(`${sourceGitignore}/${i}`, 'utf8');
     const replaceDotWithSpace = i.replace(/[.]/g, ' ');
+    const commentContent = `\n# ${replaceDotWithSpace}\n` + contents;
 
-    fs.appendFileSync(
-      `${CURR_DIR}/.gitignore`,
-      `\n# ${replaceDotWithSpace}\n` + contents
-    );
+    appendContents(pathToAppend, commentContent);
 
-    const x = i.split('.');
-    report.push(x[0].toLowerCase());
+    const [language] = i.split('.');
+    report.push(language.toLowerCase());
   });
 
   const reportMessage = `
@@ -31,7 +28,9 @@ function writing(files) {
 function add(languages) {
   if (languages.length === 0) return console.log('no language defined');
 
-  const files = getFile(languages);
+  const filesInFolder = fs.readdirSync(sourceGitignore);
+
+  const files = filesInFolder.filter(searchFile(languages));
   writing(files);
 }
 
