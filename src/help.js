c61723ba07c1206baf8b2a9da85178e213832c05
yapie <targaryen.akane@gmail.com>
2019-08-15 19:13:27 +0700

    change message help a bit

--- src/help.js
+++ src/help.js
@@ -3,10 +3,12 @@ function help() {
   command :
   add <languages> (to generate gitignore)
   list (show all list language available)
-  exist <languages>
+  exist <languages> (to check whether the languages exist)
   help
 
   im-nothing <command>
+
+  github: https://github.com/akane10/im-nothing
   `;
   console.log(message);
 }
