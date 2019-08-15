1bb75e9bf035084890f92844e38644bc5447f3ed
yapie <targaryen.akane@gmail.com>
2019-08-15 14:33:01 +0700

    toLowerCase for args

--- main.js
+++ main.js
@@ -5,13 +5,19 @@ const add = require('./src/add');
 const help = require('./src/help');
 const exist = require('./src/exist');
 
+function unique(arr) {
+  return [...new Set(arr)];
+}
+
 (function main() {
   const [_, __, command, ...args] = process.argv;
 
-  if (command === '-add') return add(args);
+  const languages = unique(args.map(i => i.toLowerCase()));
+
+  if (command === '-add') return add(languages);
   if (command === '-list') return list();
   if (command === '-help') return help();
-  if (command === '-exist') return exist(args);
+  if (command === '-exist') return exist(languages);
 
   console.log(`
   command ${command} doesn't exist
