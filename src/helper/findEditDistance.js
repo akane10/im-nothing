092d3174a6d8c88aad16475d23a2967e99fa513f
yapie <targaryen.akane@gmail.com>
2019-08-25 17:19:48 +0700

    refactor

---
+++ src/helper/findEditDistance.js
@@ -0,0 +1,46 @@
+const getEditDistance = require('./edit-distance');
+const { otherThanGitignore } = require('./helper');
+
+// findEditDistance :: [String] -> [String] -> {key: [String], key: [String]}
+function findEditDistance(notMatch, files) {
+  const splitFileName = i => i.split('.');
+  const toLower = ([name]) => name.toLowerCase();
+
+  const safeFilter = i => {
+    if (i && i.distance <= 3 && i.suggest !== '') {
+      return i;
+    }
+    return null;
+  };
+
+  const getDistance = i => {
+    return files
+      .filter(otherThanGitignore)
+      .map(splitFileName)
+      .map(toLower)
+      .map(ii => {
+        return {
+          language: i,
+          suggest: ii,
+          distance: getEditDistance(i, ii)
+        };
+      })
+      .sort((a, b) => a.distance - b.distance)
+      .map(safeFilter)
+      .filter(Boolean);
+  };
+
+  const getSuggest = ([i]) => {
+    if (i) return i.suggest;
+    return null;
+  };
+
+  const suggest = notMatch
+    .map(getDistance)
+    .map(getSuggest)
+    .filter(Boolean);
+
+  return { suggest, notMatch };
+}
+
+module.exports = findEditDistance;
---
+++ src/helper/helper.js
@@ -0,0 +1,30 @@
+const path = require('path');
+// const fs = require('fs');
+
+const joinPath = pathArg => path.join(`${__dirname}`, `${pathArg}`);
+const CURR_DIR = process.cwd();
+const sourceGitignore = joinPath(`../../gitignore`);
+
+const log = i => {
+  console.log('log', i);
+  return i;
+};
+
+const otherThanGitignore = i =>
+  i !== 'README.md' &&
+  i !== 'CONTRIBUTING.md' &&
+  i !== 'LICENSE' &&
+  i !== '.travis.yml';
+
+// isThere :: [String] -> String -> Boolean
+const isThere = languages => file => {
+  return languages.includes(file.toLowerCase());
+};
+
+module.exports = {
+  CURR_DIR,
+  sourceGitignore,
+  log,
+  otherThanGitignore,
+  isThere
+};
